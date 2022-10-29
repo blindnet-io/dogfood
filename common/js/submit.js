cc980b5a13d5f25c47bf839106c89d0f2c208939
milstan <milstan@gmail.com>
2022-10-29 14:46:36 +0200

    switch to supabase storage

--- common/js/submit.js
+++ common/js/submit.js
@@ -12,30 +12,38 @@
  *
  */
 
+
  // prepopulate form fields
 
  (new URL(window.location.href)).searchParams.forEach((x, y) =>
     document.getElementById(y).value = x)
 
+
  // handle form submission
- 
+
 const formElement = document.getElementById('form');
 
 if (formElement) {
   formElement.addEventListener('submit', (event) => {
 
-    const formData = new FormData(formElement);
+    var formData = new FormData(formElement);
+    var email = formData.get('email');
+    var name = formData.get('name');
+    var job = formData.get('job');
+    var cv = formData.get('cv');
 
-    formData.forEach(file => console.log("File: ", file));
+    const reader = new FileReader();
+
+    reader.readAsDataURL(cv);
+
 
     // register consent
-
 
     fetch('https://devkit-pce-staging.azurewebsites.net/v0/user-events/consent', {
       method: 'POST',
       body: JSON.stringify({
           dataSubject: {
-            id: formData.get('email'),
+            id: email,
             schema: 'dsid',
           },
           //consent ID for talent portal
@@ -48,14 +56,31 @@ if (formElement) {
         console.log(`consent registered`);
 
         // submit data
-        fetch('https://blindnet-connector-demo-staging.azurewebsites.net/form', {
+        const headers = new Headers();
+        headers.append('apikey','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imppcnh0emFtbHZ3YW11eWlnem1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY5Nzk0MjcsImV4cCI6MTk4MjU1NTQyN30.GBTO_ckSgTU4EKpvXUOy1GkWlnw_FoZZ-6s8cpNfv1E');
+        headers.append('Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imppcnh0emFtbHZ3YW11eWlnem1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY5Nzk0MjcsImV4cCI6MTk4MjU1NTQyN30.GBTO_ckSgTU4EKpvXUOy1GkWlnw_FoZZ-6s8cpNfv1E');
+        headers.append('Content-Type','application/json');
+        headers.append('Prefer','return=representation');
+
+        var object = {};
+        formData.forEach((value, key) => object[key] = value);
+        object['cv'] = reader.result;
+        console.log(object['cv']);
+        var body = JSON.stringify(object);
+
+        // send data
+        fetch('https://jirxtzamlvwamuyigzmj.supabase.co/rest/v1/cv-sumbissions', {
           method: 'POST',
-          body: formData
+          headers: headers,
+          body: body
         })
         .then((response) => {
           if (response.ok) {
-            console.log(`OK`);
+            console.log(`submission OK`);
             window.location.href = './thanx.html';
+          } else {
+            console.log(`submission NOT OK`);
+            window.location.href = './whoops.html';
           }
         })
         .then(result => {
@@ -64,7 +89,7 @@ if (formElement) {
         })
         .catch(error => {
           console.error('Error:', error);
-        });
+        })
 
 
       }
